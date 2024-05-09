38bb6626d15f269441c38c47b9645e4e83d4d5db
qureshiowais <qureshiowais91@gmail.com>
2024-05-09 14:53:16 +0530

    reset password

--- routes/validation.js
+++ routes/validation.js
@@ -1,9 +1,14 @@
-const { sendOtp, validateOTP } = require('../controllers/validation');
+const {
+  sendOtp,
+  validateOTP,
+  resetpassword,
+} = require('../controllers/validation');
 const express = require('express');
-const asyncHandler = require("../middleware/asynchandler")
+const asyncHandler = require('../middleware/asynchandler');
 const validation = express.Router();
 
 validation.post('/resetpassword', sendOtp);
 validation.post('/validateotp', asyncHandler(validateOTP));
+validation.post('/reset', asyncHandler(resetpassword));
 
 module.exports = { validation };

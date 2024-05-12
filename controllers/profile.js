a5cacb9e8dc604f863532268ef73ac16ce04a9f6
qureshiowais <qureshiowais91@gmail.com>
2024-05-12 12:15:38 +0530

    profile  for user and related school

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,25 +1,47 @@
 const User = require('../models/User');
+const CustomError = require('../util/error');
 
 // Controller function for the getProfile endpoint
+// return school and full profile
 async function getProfile(req, res) {
-    const userId = req.user.userId;
+  const userId = req.user.userId;
 
-    try {
-        // Find the user by userId and populate the 'school' field
-        const user = await User.findById(userId).populate('school');
+  if (!userId) {
+    return new CustomError('User Id Missing', 404);
+  }
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
+  // Find the user by userId and populate the 'school' field
+  const user = await User.findById(userId).populate('school');
 
-        // Return the user profile, including the populated 'school' field
-        res.status(200).json(user);
-    } catch (error) {
-        console.error("Error fetching user profile:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
+  if (!user) {
+    return new CustomError('User Not Found', 404);
+  }
+
+  res.status(200).json(user);
+}
+
+// select school and only return it
+async function getRelatedSchoolInfo(req, res) {
+  const userId = req.user.userId;
+
+  if (!userId) {
+    return new CustomError('User Id Missing', 404);
+  }
+
+  const user = await User.findById(userId)
+    .populate({
+      path: 'school',
+    })
+    .select('-_id school');
+
+  if (!user?.school) {
+    throw new CustomError('School Not Found in User Object', 404);
+  }
+
+  res.status(200).json({ school: user?.school });
 }
 
 module.exports = {
-    getProfile
+  getProfile,
+  getRelatedSchoolInfo,
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const profile = express.Router();
-const authenticateToken = require("../middleware/authentication")
-const { getProfile } = require('../controllers/profile');
+const { getProfile, getRelatedSchoolInfo } = require('../controllers/profile');
 
-profile.get('/profile', authenticateToken, getProfile);
+// Middlewares
+const asyncHandler = require('../middleware/asynchandler');
+const authorizeRoles = require('../middleware/authorization');
+const authenticateToken = require('../middleware/authentication');
+
+profile.get(
+  '/school/profile',
+  authenticateToken,
+  asyncHandler(getRelatedSchoolInfo)
+);
+profile.get(
+  '/user/profile',
+  authenticateToken,
+  asyncHandler(getProfile)
+);
 
 module.exports = { profile };

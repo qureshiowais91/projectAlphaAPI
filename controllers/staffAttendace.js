55b64701cd84bb4fc4a4522cca4cc10e36c5ace4
qureshiowais <qureshiowais91@gmail.com>
2024-06-12 19:46:23 +0530

    correction

--- controllers/staffAttendace.js
+++ controllers/staffAttendace.js
@@ -2,44 +2,45 @@ const StaffAttendance = require('../models/staffAttendance'); // Importing the S
 
 // Controller function to create new attendance based on school ID
 const createAttendance = async (req, res) => {
-    const { schoolId, teacherId, approximateLocation } = req.body;
+  console.log(req.user._id);
 
-    try {
-        // Get today's date
-        const today = new Date();
-        today.setHours(5, 0, 0, 0); // Set hours to 5:00 AM, minutes, seconds, and milliseconds to 0
+  const { schoolId, teacherId, approximateLocation } = req.body;
 
-        // Find existing attendance for the same day, school ID, and teacher ID
-        let attendance = await StaffAttendance.findOne({
-            schoolId: schoolId,
-            teacherId: teacherId,
-            loginTime: { $gte: today }
-        });
+  try {
+    // Get today's date
+    const today = new Date();
+    today.setHours(5, 0, 0, 0); // Set hours to 5:00 AM, minutes, seconds, and milliseconds to 0
 
-        if (attendance) {
-            // If attendance exists for the same day, school ID, and teacher ID, update it with new attendance log
-            res.status(201).json({ message: 'Attendance Alread Exist'});
-        } else {
-            // If no attendance exists, create a new attendance record
-            attendance = new StaffAttendance({
-                schoolId: schoolId,
-                teacherId: teacherId,
-                approximateLocation: approximateLocation
-            });
+    // Find existing attendance for the same day, school ID, and teacher ID
+    let attendance = await StaffAttendance.findOne({
+      schoolId: schoolId,
+      teacherId: teacherId,
+      loginTime: { $gte: today },
+    });
 
-            await attendance.save(); // Save the new attendance record
-        }
-
-        // Send success response
-        res.status(201).json({ message: 'Attendance record created successfully.', attendance: attendance });
-
-    } catch (error) {
-        // Handle errors
-        console.error('Error creating attendance:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    if (attendance) {
+      // If attendance exists for the same day, school ID, and teacher ID, update it with new attendance log
+      res.status(201).json({ message: 'Attendance Alread Exist' });
+    } else {
+      // If no attendance exists, create a new attendance record
+      attendance = await StaffAttendance.create({
+        schoolId: schoolId,
+        teacherId: teacherId,
+        attendanceLogs: [{ approximateLocation: approximateLocation }],
+      }); // Save the new attendance record
+      // Send success response
+      res.status(201).json({
+        message: 'Attendance record created successfully.',
+        attendance: attendance,
+      });
     }
+  } catch (error) {
+    // Handle errors
+    console.error('Error creating attendance:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 module.exports = {
-    createAttendance
+  createAttendance,
 };
